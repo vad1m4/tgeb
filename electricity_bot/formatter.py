3c0bbb1ef00344ac5b8a8456f0bad5f219d7da6a
vad1m4 <vad.levitskiy@gmail.com>
2024-07-02 00:22:58 +0300

    0.31

--- electricity_bot/formatter.py
+++ electricity_bot/formatter.py
@@ -7,7 +7,6 @@ def format_text(time: int, measurement: str) -> str:
     if (time % 10) > 1 and (time % 10) < 5 and time > 20:
         measurement += "и"
     return measurement
-        
 
 
 def format(unix: int) -> str:
@@ -17,20 +16,17 @@ def format(unix: int) -> str:
     seconds = int(seconds)
 
     components = []
-        
+
     if hours > 0:
-        components.append(f"{hours} {format_text(hours, "годин")}")
-    
+        components.append(f"{hours} {format_text(hours, 'хвилин')}")
+
     if minutes > 0:
-        components.append(f"{minutes} {format_text(minutes, "хвилин")}")
-    
+        components.append(f"{minutes} {format_text(minutes, 'хвилин')}")
+
     if seconds > 0:
-        components.append(f"{seconds} {format_text(seconds, "секунд")}")
-    
+        components.append(f"{seconds} {format_text(seconds, 'хвилин')}")
+
     if len(components) == 1:
         return components[0]
     else:
         return ", ".join(components[:-1]) + f" й {components[-1]}"
-    
-
-
